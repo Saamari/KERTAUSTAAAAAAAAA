385eaadf3c8dea3cfbaae54c530df92f2bfe8d63
Saamari <sampsa_kautto@hotmail.com>
2016-02-12 15:56:02 +0200

    KERTAUSSS

---
+++ Kertaustunti/Henkilo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaustunti
+{
+    class Henkilo
+    {
+        public string Nimi { get; set; }
+        private double paino; //0-100 KG kapselointi!!!!!
+        public double Paino
+        {
+            get
+            {
+                return paino;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100) paino = value;
+                else
+                {
+                    Console.WriteLine("Pelaajan paino ei ole sallittu");
+                    paino = 0;
+                }
+            }
+
+        }
+
+
+        public Henkilo()
+        {
+            
+        }
+
+        public Henkilo(string nimi, double paino)
+        {
+            Nimi = nimi;
+            Paino = paino;
+        }
+
+        public override string ToString()
+        {
+            return Nimi + " " + paino;
+        }
+
+
+    } // Class 
+
+} // namspace
---
+++ Kertaustunti/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaustunti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilo henkilo = new Henkilo();
+            henkilo.Nimi = "MAtti  Knatterton";
+            henkilo.Paino = 200;
+            Console.WriteLine("Henkilön nimi on : " + henkilo.Nimi);
+            Console.WriteLine("Henkilön paino on : " + henkilo.Paino);
+
+            string nimi = "Merimies Niksu";
+            double paino = 50;
+
+            Henkilo henkilo2 = new Henkilo(nimi , paino);
+            Console.WriteLine("Henkilön nimi on : " + henkilo2.Nimi);
+            Console.WriteLine("Henkilön paino on : " + henkilo2.Paino);
+
+            Henkilo henkilo3 = new Henkilo { Nimi = "Khulli", Paino = 11 };
+
+            Katsoja katsoja = new Katsoja("Jaska",  100 ,  true);
+            Console.WriteLine(katsoja.ToString());
+            katsoja.huutaa();
+            
+
+
+
+        }
+    }
+}
